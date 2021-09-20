d9e8be11a76e6c39a9864175938fd417569682f9
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-21 02:09:15 +0300

    Create Product Controller index, create page blade.php for /products, also change pagination style to use bootstrap and change site where images are taken

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,9 @@ class ProductsController extends Controller
 {
     public function index()
     {
-
+        //with - подтягиваем информацию сразу и категорий продуктов
+        $products = Product::query()->with('category')->paginate(12);
+        return view('products/index', compact('products'));
     }
 
     public function show(Product $product)
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('products', [\App\Http\Controllers\ProductsController::class, 'index'])->name('products');
 Route::get('products/{product}', [\App\Http\Controllers\ProductsController::class, 'show'])->name('products.show');
