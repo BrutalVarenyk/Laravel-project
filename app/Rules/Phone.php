0cf43cf04ba0236a582aa5f04282ab27b8429d7b
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-23 22:39:49 +0300

    Create Rule for phone validation, for validation using pattern

---
+++ app/Rules/Phone.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Phone implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        preg_match('/\+[0-9]{1,3}\([0-9]{2,3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}/', $value, $matches);
+
+        return !empty($matches);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The pattern of phone number have to be equal "+XX(XXX)XXX-XX-XX".';
+    }
+}
