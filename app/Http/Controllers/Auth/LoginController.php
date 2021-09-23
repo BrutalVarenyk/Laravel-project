4d2468aabc50c64c3147fd32c9ae9ba201ac44d8
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-24 01:04:48 +0300

    create redirectPath to admin if user is admin or to home page for customer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,12 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectPath()
+    {
+//        dd(auth()->user()->role->isAdmin());
+        return auth()->user()->role->isAdmin() ? route('admin.home') : $this->redirectTo;
+    }
 
     /**
      * Create a new controller instance.
