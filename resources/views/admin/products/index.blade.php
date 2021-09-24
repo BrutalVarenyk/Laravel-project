f2dd6b00251ebf78c7cc9061e40001351d2592b9
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-25 00:24:54 +0300

    Create admin.produrcts page with list of products, buttons to edit delete and view product

---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h3 class="text-center">{{ __('Products') }}</h3>
+            </div>
+            <div class="col-md-12">
+                <table class="table align-self-center">
+                    <thread>
+                        <tr>
+                            <th class="text-center" scope="col">ID</th>
+                            <th class="text-center" scope="col">Thumbnail</th>
+                            <th class="text-center" scope="col">Name</th>
+                            <th class="text-center" scope="col">Quantity</th>
+                            <th class="text-center" scope="col">Category</th>
+                            <th class="text-center" scope="col">Actions</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        @foreach($products as $product)
+                            <tr>
+                                <td class="text-center" scope="col">{{ $product->id }}</td>
+                                <td class="text-center" scope="col"><img src="{{ $product->thumbnail }}" width="90" alt="{{ $product->title }}"></td>
+                                <td class="text-center" scope="col">{{ $product->title }}</td>
+                                <td class="text-center" scope="col">{{ $product->in_stock }}</td>
+                                <td class="text-center" scope="col">@include('categories.parts.category_view', ['category' => $product->category])</td>
+                                <td class="text-center" scope="col">
+                                    <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-info form-control">Edit</a>
+                                    <form action="{{ route('admin.products.delete', $product->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" class="btn btn-danger form-control" value="Remove">
+                                    </form>
+                                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-outline-success form-control">View</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="col-md-8">
+                    {{ $products->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
