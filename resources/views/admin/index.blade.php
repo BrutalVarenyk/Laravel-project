c976deaffab92ab1f469e4d197957a7f546a2e81
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-25 00:23:41 +0300

    Create admin.home page with inspiring greeting)

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h3 class="text-center">{{ __('You are admin, great job!') }}</h3>
+            </div>
+        </div>
+    </div>
+@endsection
