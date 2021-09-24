50d754adbbf67cecda439878c40ffa0a083c4cc0
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-25 00:27:07 +0300

    Create different navigations bars for user/guest and admin
    Main (for user/guest) nav bar was remake in front-blade and delete in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,77 +30,15 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <a onclick="goBack()" class="btn btn-default">< Back</a>
-            <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Home
-                    </a>
-                    <a class="navbar-brand" href="{{ url('products/')  }}">
-                        Products
-                    </a>
-                <div class="dropdown">
-                    <a class="navbar-brand dropdown" href="#"  data-toggle="dropdown" >
-                        Categories
-                    </a>
-                    <div class="dropdown-menu">
-                        <a href="{{ url('categories/') }}" class="dropdown-item">All Categories</a>
-                        <div class="dropdown-divider"></div>
-                        @foreach($all_categories::getAllCategories() as $category)
-                            <a href="{{ route('categories.show', $category->id) }}" class="dropdown-item">{{ $category->name }}</a>
-                        @endforeach
-                    </div>
-                </div>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+        @if(!Auth::user())
+            @include('navigation.front-menu')
+        @else
+            @if(is_admin(Auth::user()) && (Request::is('admin/*') || Request::is('admin')) )
+                @include('navigation.admin-menu')
+            @else
+                @include('navigation.front-menu')
+            @endif
+        @endif
         <main class="py-4">
             <div class="row">
                 <div class="col-md-12">
@@ -111,8 +49,8 @@
                     @endif
                 </div>
             </div>
-            @yield('content')
         </main>
+        @yield('content')
     </div>
 </body>
 </html>
---
+++ resources/views/navigation/admin-menu.blade.php
@@ -0,0 +1,97 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <a onclick="goBack()" class="btn btn-default">< Back</a>
+    <div class="container">
+        <div class="collapse navbar-collapse">
+            <a class="navbar-brand" href="{{ route('admin.home') }}">{{ __('Admin Panel') }}</a>
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item dropdown">
+                    <a id="adminDropdownOrders" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="">
+                        {{ __('Categories') }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="{{ url('categories/') }}" class="dropdown-item">All Categories</a>
+                        <div class="dropdown-divider"></div>
+                        @foreach($all_categories::getAllCategories() as $category)
+                            <a href="{{ route('categories.show', $category->id) }}" class="dropdown-item">{{ $category->name }}</a>
+                        @endforeach
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a id="adminDropdownOrders" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="">
+                        {{ __('Orders') }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-left">
+                        <a class="dropdown-item" href="{{route('admin.orders')}}">{{ __('Orders List') }}</a>
+                        <a class="dropdown-item" href="{{route('admin.orders')}}">{{ __('New Order') }}</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a id="adminDropdownProducts" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="">
+                        {{ __('Products') }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-left">
+                        <a class="dropdown-item" href="{{route('admin.products')}}">{{ __('Products List') }}</a>
+                        <a class="dropdown-item" href="{{route('admin.products')}}">{{ __('New Product') }}</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ url('products/')  }}">
+                            {{ __('Products') }}
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('admin.home') }}">
+                                    {{ __('Admin') }}
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+            </ul>
+{{--            <ul class="navbar-nav mr-auto">--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="navbar-brand" href="{{ route('admin.home') }}">{{ __('Admin Panel:') }}</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--            <ul class="navbar-nav mr-auto">--}}
+{{--                <li class="nav-item dropdown">--}}
+{{--                    <a id="adminDropdownOrders" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="">--}}
+{{--                        {{ __('Orders') }} <span class="caret"></span>--}}
+{{--                    </a>--}}
+{{--                    <div class="dropdown-menu dropdown-menu-left">--}}
+{{--                        <a class="dropdown-item" href="{{route('admin.orders')}}">{{ __('Orders List') }}</a>--}}
+{{--                        <a class="dropdown-item" href="{{route('admin.orders')}}">{{ __('New Order') }}</a>--}}
+{{--                    </div>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item dropdown">--}}
+{{--                    <a id="adminDropdownProducts" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="">--}}
+{{--                        {{ __('Products') }} <span class="caret"></span>--}}
+{{--                    </a>--}}
+{{--                    <div class="dropdown-menu dropdown-menu-left">--}}
+{{--                        <a class="dropdown-item" href="{{route('admin.products')}}">{{ __('Products List') }}</a>--}}
+{{--                        <a class="dropdown-item" href="{{route('admin.products')}}">{{ __('New Product') }}</a>--}}
+{{--                    </div>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+        </div>
+    </div>
+</nav>
