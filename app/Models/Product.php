a91d65687eab08e01a5fe8bcecb0485f0470dfec
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-26 00:31:00 +0300

    Change mistake in fillable property

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-        'id',
+        'category_id',
         'title',
         'description',
         'short_description',
