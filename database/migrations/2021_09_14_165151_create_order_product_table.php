a453959b6eab07a13c404b5ab093b7ee0241319b
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-09-26 00:38:51 +0300

    Added ->onDelete('cascade') for each foreign key for easier deleting

--- database/migrations/2021_09_14_165151_create_order_product_table.php
+++ database/migrations/2021_09_14_165151_create_order_product_table.php
@@ -16,8 +16,8 @@ class CreateOrderProductTable extends Migration
         Schema::create('order_product', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('order_id')->constrained('orders');
-            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
 
             //pivot fields
             $table->unsignedInteger('quantity');
