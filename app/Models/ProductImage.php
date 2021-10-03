1bd6f0f139101c90d852832d356d5dd19b43951d
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-10-04 02:27:39 +0300

    Add mutator to path attribute which adding path with image service

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\Images\ImageService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,11 @@ class ProductImage extends Model
     public function products()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    // Mutator
+    public function setPathAttribute($image)
+    {
+        $this->attributes['path'] = ImageService::upload($image);
     }
 }
