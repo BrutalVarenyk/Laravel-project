d1005875cd7551d46a73c1b4e0d61967a7471d16
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-10-05 16:45:30 +0300

    Create ProductImagesService with method attach which upload physically image on server, then create a record in ProductImage table and attach it to product

---
+++ app/Services/Images/ProductImagesService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Images;
+
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Support\Facades\Storage;
+
+class ProductImagesService implements ProductImagesServiceInterface
+{
+
+    public static function attach(Product $product, array $images = [])
+    {
+        if (!empty($image)) {
+            foreach ($images as $image) {
+                $path = ImageService::upload($image);
+                $newImage = ProductImage::create(['path' => $path]);
+                $product->gallery()->attach($newImage);
+            }
+        }
+    }
+}
