ecbbac15b556a978aecfb09eb7b90b2c902465d3
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-10-17 13:55:49 +0300

    Add fillable values

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -10,6 +10,8 @@ class ProductImage extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['path', 'product_id'];
+
     public function products()
     {
         return $this->belongsTo(Product::class);
