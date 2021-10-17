7f9a124efb491fdd9a19cfb2cb3e21f68765fb80
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-10-17 14:00:25 +0300

    Added remove method

--- app/Services/Images/ImageService.php
+++ app/Services/Images/ImageService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Images;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -27,6 +28,11 @@ class ImageService implements ImageServiceInterface
             . (!$is_string ? $image->getClientOriginalExtension() : $imageData[1]);
 
         // save image with our random unique path
+//        Storage::put(
+//            $imagePath,
+//            Storage::url($image)
+//        );
+
         Storage::put(
             $imagePath,
             File::get($image)
@@ -38,5 +44,35 @@ class ImageService implements ImageServiceInterface
     public static function remove($image)
     {
         Storage::delete($image);
+
+        $path = explode('/', $image);
+        array_pop($path);
+
+        $FileSystem = new Filesystem();
+
+        while ($path[array_key_last($path)] !== 'public')
+        {
+            // Target directory.
+            $directory = storage_path() . '/app/' . implode('/', $path);
+
+            // Check if the directory exists.
+            if ($FileSystem->exists($directory)) {
+
+                // Get all files in this directory.
+                $files = $FileSystem->files($directory);
+
+                // Check if directory is empty.
+                if (empty($files)) {
+                    // Yes, delete the directory.
+                    $FileSystem->deleteDirectory($directory);
+
+                    array_pop($path);
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
     }
 }
