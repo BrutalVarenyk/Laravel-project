e20637428ebd12bcf12d74316ba0bbd769b8d725
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-10-23 20:53:30 +0300

    Add collumns for description and actions

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -15,14 +15,31 @@
                                     <thead>
                                     <tr>
                                         <th class="text-center" scope="col"> {{ __('Category Name') }}</th>
+                                        <th class="text-center" scope="col"> {{ __('Category Description') }}</th>
                                         <th class="text-center" scope="col"> {{ __('Products count') }}</th>
+                                        @if(is_admin(auth()->user()))
+                                            <th class="text-center" scope="col"> {{ __('Actions') }}</th>
+                                        @endif
                                     </tr>
                                     </thead>
                                     <tbody>
                                     @foreach($categories as $category)
                                         <tr>
-                                            <th class="text-center"> @include('categories.parts.category_view', ['category' => $category])</th>
-                                            <th class="text-center"> {{ $category->products_count}}</th>
+                                            <td class="text-center"> @include('categories.parts.category_view', ['category' => $category])</td>
+                                            <td class="text-center"> {{ $category->description}}</td>
+                                            <td class="text-center"> {{ $category->products_count}}</td>
+
+                                            @if(is_admin(auth()->user()))
+                                                <td class="text-center" scope="col">
+                                                    <a href="{{ route('lang.admin.categories.edit', $category->id) }}" class="btn btn-info form-control">{{__('Edit')}}</a>
+                                                    <form action="{{ route('lang.admin.categories.delete', $category->id) }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <input type="submit" class="btn btn-danger form-control" value="{{__('Remove')}}">
+                                                    </form>
+                                                    <a href="{{ route('lang.categories.show', $category->id) }}" class="btn btn-outline-success form-control">{{__('View')}}</a>
+                                                </td>
+                                            @endif
                                         </tr>
                                     @endforeach
                                     </tbody>
