31e5ec211c94e990fdade1dbfac2739797a2af79
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-11-03 22:25:53 +0200

    Added routes for wishlist( view, add and delete)

--- routes/web.php
+++ routes/web.php
@@ -45,35 +45,37 @@ Route::prefix(\App\Services\Localization\LocalizationService::localize())
             ->name('edit'); // lang.account.edit
         Route::put('/update', [\App\Http\Controllers\Account\UserController::class, 'update'])
             ->name('update'); // lang.account.update
+        Route::get('/wishlist', [\App\Http\Controllers\Account\WishListController::class, 'index'])
+            ->name('wishlist');
     });
 
     Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('auth', 'admin')
         ->group(function (){
 
-        Route::get('/', '\App\Http\Controllers\Admin\BoardController')
-            ->name('home'); // lang.admin.home
+            Route::get('/', '\App\Http\Controllers\Admin\BoardController')
+                ->name('home'); // lang.admin.home
 
-        Route::name('orders')->group(function (){
-            Route::get('orders', [\App\Http\Controllers\Admin\OrdersController::class, 'index']); // lang.admin.orders
-            Route::get('orders/{order}/edit', [\App\Http\Controllers\Admin\OrdersController::class, 'edit'])
-                ->name('.edit'); // lang.admin.orders.edit
-            Route::get('orders/{order}/show', [\App\Http\Controllers\Admin\OrdersController::class, 'show'])
-                ->name('.show'); // lang.admin.orders.show
-        });
-
-        Route::name('products')->group(function (){
-            Route::get('products', [\App\Http\Controllers\Admin\ProductsController::class, 'index']); //admin.products
-            Route::get('products/{product}/edit', [\App\Http\Controllers\Admin\ProductsController::class, 'edit'])
-                ->name('.edit'); // lang.admin.products.edit
-            Route::put('products/{product}/update', [\App\Http\Controllers\Admin\ProductsController::class, 'update'])
-                ->name('.update'); // lang.admin.products.edit
-            Route::delete('products/{product}/delete', [\App\Http\Controllers\Admin\ProductsController::class, 'destroy'])
-                ->name('.delete'); // lang.admin.products.edit
-            Route::get('products/new', [\App\Http\Controllers\Admin\ProductsController::class, 'create'])
-                ->name('.create'); // lang.admin.products.create
-            Route::post('products', [\App\Http\Controllers\Admin\ProductsController::class, 'store'])
-                ->name('.store'); // lang.admin.products.store
+            Route::name('orders')->group(function (){
+                Route::get('orders', [\App\Http\Controllers\Admin\OrdersController::class, 'index']); // lang.admin.orders
+                Route::get('orders/{order}/edit', [\App\Http\Controllers\Admin\OrdersController::class, 'edit'])
+                    ->name('.edit'); // lang.admin.orders.edit
+                Route::get('orders/{order}/show', [\App\Http\Controllers\Admin\OrdersController::class, 'show'])
+                    ->name('.show'); // lang.admin.orders.show
             });
+
+            Route::name('products')->group(function (){
+                Route::get('products', [\App\Http\Controllers\Admin\ProductsController::class, 'index']); //admin.products
+                Route::get('products/{product}/edit', [\App\Http\Controllers\Admin\ProductsController::class, 'edit'])
+                    ->name('.edit'); // lang.admin.products.edit
+                Route::put('products/{product}/update', [\App\Http\Controllers\Admin\ProductsController::class, 'update'])
+                    ->name('.update'); // lang.admin.products.edit
+                Route::delete('products/{product}/delete', [\App\Http\Controllers\Admin\ProductsController::class, 'destroy'])
+                    ->name('.delete'); // lang.admin.products.edit
+                Route::get('products/new', [\App\Http\Controllers\Admin\ProductsController::class, 'create'])
+                    ->name('.create'); // lang.admin.products.create
+                Route::post('products', [\App\Http\Controllers\Admin\ProductsController::class, 'store'])
+                    ->name('.store'); // lang.admin.products.store
+                });
 
             Route::name('categories')->group(function (){
                 Route::get('categories/{category}/edit', [\App\Http\Controllers\Admin\CategoriesController::class, 'edit'])
@@ -105,6 +107,12 @@ Route::prefix(\App\Services\Localization\LocalizationService::localize())
 
         Route::post('order', [\App\Http\Controllers\OrdersController::class, 'store'])
             ->name('order.create');
+
+        Route::get('wishlist/{product}/add', [\App\Http\Controllers\WishListController::class, 'add'])
+            ->name('wishlist.add');
+
+        Route::delete('wishlist/{product}/delete', [\App\Http\Controllers\WishListController::class, 'delete'])
+            ->name('wishlist.delete');
     });
 });
 
