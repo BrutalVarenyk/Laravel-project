6ee3a8d8949c973aa81eab447edd7e76b2ec78b3
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-11-05 05:15:17 +0200

    Add possibility to rate by stars to product show blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -69,42 +69,40 @@
                             </form>
                         </div>
                     @endif
-{{--                    <form class="form-horizontal poststars" action="{{ route('rating.add', $product) }}" id="addStar"--}}
-{{--                          method="POST">--}}
-{{--                        @csrf--}}
-{{--                        <div class="form-group required">--}}
-{{--                            <div class="col-sm-12 stars">--}}
-{{--                                @if(!is_null($product->getUserRatingForCurrentProduct()))--}}
-{{--                                    @for($i = 5; $i >= 1; $i--)--}}
-{{--                                        <input class="star star-{{$i}}"--}}
-{{--                                               value="{{$i}}"--}}
-{{--                                               id="star-{{$i}}"--}}
-{{--                                               type="radio"--}}
-{{--                                               name="star"--}}
-{{--                                            {{--}}
-{{--                                            $i == $product->getUserRatingForCurrentProduct()->rating--}}
-{{--                                            ? 'checked'--}}
-{{--                                            : ''--}}
-{{--                                            }}--}}
-{{--                                        />--}}
-{{--                                        <label class="star star-{{$i}}" for="star-{{$i}}"></label>--}}
-{{--                                    @endfor--}}
-{{--                                @else--}}
-{{--                                    <input class="star star-5" value="5" id="star-5" type="radio" name="star"/>--}}
-{{--                                    <label class="star star-5" for="star-5"></label>--}}
-{{--                                    <input class="star star-4" value="4" id="star-4" type="radio" name="star"/>--}}
-{{--                                    <label class="star star-4" for="star-4"></label>--}}
-{{--                                    <input class="star star-3" value="3" id="star-3" type="radio" name="star"/>--}}
-{{--                                    <label class="star star-3" for="star-3"></label>--}}
-{{--                                    <input class="star star-2" value="2" id="star-2" type="radio" name="star"/>--}}
-{{--                                    <label class="star star-2" for="star-2"></label>--}}
-{{--                                    <input class="star star-1" value="1" id="star-1" type="radio" name="star"/>--}}
-{{--                                    <label class="star star-1" for="star-1"></label>--}}
-{{--                                @endif--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                    <hr>--}}
+                    <div class="form-horizontal poststars" data-route="{{ route('lang.rating.add', $product) }}" id="addStar">
+                        <div class="form-group required">
+                            <div class="col-sm-12 stars">
+                                @if(!is_null($product->getUserRatingForCurrentProduct()))
+                                    @for($i = 5; $i >= 1; $i--)
+                                        <input class="star star-{{$i}}"
+                                               value="{{$i}}"
+                                               id="star-{{$i}}"
+                                               type="radio"
+                                               name="star"
+                                            {{
+                                            $i == $product->getUserRatingForCurrentProduct()->rating
+                                            ? 'checked'
+                                            : ''
+                                            }}
+                                        />
+                                        <label class="star star-{{$i}}" for="star-{{$i}}"></label>
+                                    @endfor
+                                @else
+                                    <input class="star star-5 " value="5" id="star-5" type="radio" name="star"/>
+                                    <label class="star star-5" for="star-5"></label>
+                                    <input class="star star-4" value="4" id="star-4" type="radio" name="star"/>
+                                    <label class="star star-4" for="star-4"></label>
+                                    <input class="star star-3" value="3" id="star-3" type="radio" name="star"/>
+                                    <label class="star star-3" for="star-3"></label>
+                                    <input class="star star-2" value="2" id="star-2" type="radio" name="star"/>
+                                    <label class="star star-2" for="star-2"></label>
+                                    <input class="star star-1" value="1" id="star-1" type="radio" name="star"/>
+                                    <label class="star star-1" for="star-1"></label>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
                     @if($wishlist->isUserFollowed($product))
                         <form action="{{ route('lang.wishlist.delete', $product) }}" method="POST">
                             @csrf
@@ -126,4 +124,5 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/product-rating.js') }}" type="text/javascript"></script>
 @endsection
