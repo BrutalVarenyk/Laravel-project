d14c6b0d5a510cf01709bcb583fd7462b76a54f4
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-11-05 05:18:16 +0200

    Create controller with method add where add mark of user to product and response back

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use willvincent\Rateable\Rating;
+
+class RatingController extends Controller
+{
+    public function add(Request $request, Product $product)
+    {
+        $product->rateOnce($request->starId);
+        return response()->json(['success']);
+    }
+}
