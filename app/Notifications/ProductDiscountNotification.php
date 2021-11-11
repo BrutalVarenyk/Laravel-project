68c897973551fd38b8eacd42cd54bf77033e2f75
BrutalVarenyk <brutalvarenyk@gmail.com>
2021-11-11 19:48:17 +0200

    discount notification for those who follow the product

---
+++ app/Notifications/ProductDiscountNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProductDiscountNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $product;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        logs()->info('App\Notifications\ProductAppearedNotification  via');
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = route('lang.products.show', $this->product);
+        return (new MailMessage)
+            ->line("The product {$this->product->title} from your wishlist")
+            ->line("have got discount -{$this->product->discount}%")
+            ->action('Do not miss it', $url)
+            ->line('Thank you for using our shop!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
